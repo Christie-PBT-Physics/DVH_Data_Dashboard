b3b56d4dc7fd1a9ff8662d018d11538af9e2f6a2
John-William Warmenhoven <john.warmenhoven@manchester.ac.uk>
2025-12-02 16:04:39 +0000

    Add Group Selection Buttons to Contour Selection
    Added buttons to Automatically highlight all, treatment contours, or OAR contours

--- DataDashboard.py
+++ DataDashboard.py
@@ -96,7 +96,6 @@ def plot_single_violin_plotly(all_data, patient, patient_id, metric, oar):
                             name='Selected Patient',
                             zorder=3)
     points_df = swarm_df(all_data[all_data['OAR']==oar][['Patient', metric]], metric)
-    print(points_df)
     points = go.Scatter(x=points_df['x'],
                             y=points_df['y'],
                             marker=dict(color='#abd9e9', symbol='circle-open'),
@@ -130,112 +129,6 @@ def negative_1_if_count_is_odd(count):
         return -1
     else:
         return 1
-
-def swarm(
-    X_series,
-    fig_title,
-    point_size=2,
-    fig_width=800,
-    gap_multiplier=1.2,
-    bin_fraction=0.95,  # slightly undersizes the bins to avoid collisions
-):
-    # sorting will align columns in attractive c-shaped arcs rather than having
-    # columns that vary unpredictably in the x-dimension.
-    # We also exploit the fact that sorting means we see bins sequentially when
-    # we add collision prevention offsets.
-    X_series = X_series.copy().sort_values()
-
-    # we need to reason in terms of the marker size that is measured in px
-    # so we need to think about each x-coordinate as being a fraction of the way from the
-    # minimum X value to the maximum X value
-    min_x = min(X_series)
-    max_x = max(X_series)
-
-    list_of_rows = []
-    # we will count the number of points in each "bin" / vertical strip of the graph
-    # to be able to assign a y-coordinate that avoids overlapping
-    bin_counter = collections.Counter()
-
-    for x_val in X_series:
-        # assign this x_value to bin number
-        # each bin is a vertical strip slightly narrower than one marker
-        bin = (((fig_width*bin_fraction*(x_val-min_x))/(max_x-min_x)) // point_size)
-
-        # update the count of dots in that strip
-        bin_counter.update([bin])
-
-        # remember the "y-slot" which tells us the number of points in this bin and is sufficient to compute the y coordinate unless there's a collision with the point to its left
-        list_of_rows.append(
-            {"x": x_val, "y_slot": bin_counter[bin], "bin": bin})
-
-    # iterate through the points and "offset" any that are colliding with a
-    # point to their left apply the offsets to all subsequent points in the same bin.
-    # this arranges points in an attractive swarm c-curve where the points
-    # toward the edges are (weakly) further right.
-    bin = 0
-    offset = 0
-    for row in list_of_rows:
-        if bin != row["bin"]:
-            # we have moved to a new bin, so we need to reset the offset
-            bin = row["bin"]
-            offset = 0
-        # see if we need to "look left" to avoid a possible collision
-        for other_row in list_of_rows:
-            if (other_row["bin"] == bin-1):
-                # "bubble" the entry up until we find a slot that avoids a collision
-                while ((other_row["y_slot"] == row["y_slot"]+offset) and (((fig_width*(row["x"]-other_row["x"]))/(max_x-min_x) // point_size) < 1)):
-                    offset += 1
-                    # update the bin count so we know whether the number of
-                    # *used* slots is even or odd
-                    bin_counter.update([bin])
-
-        row["y_slot"] += offset
-        # The collision free y coordinate gives the items in a vertical bin
-        # y-coordinates to evenly spread their locations above and below the
-        # y-axis (we'll make a correction below to deal with even numbers of
-        # entries).  For now, we'll assign 0, 1, -1, 2, -2, 3, -3 ... and so on.
-        # We scale this by the point_size*gap_multiplier to get a y coordinate
-        # in px.
-        row["y"] = (row["y_slot"]//2) * \
-            negative_1_if_count_is_odd(row["y_slot"])*point_size*gap_multiplier
-
-    # if the number of points is even, move y-coordinates down to put an equal
-    # number of entries above and below the axis
-    for row in list_of_rows:
-        if bin_counter[row["bin"]] % 2 == 0:
-            row["y"] -= point_size*gap_multiplier/2
-
-    df = pd.DataFrame(list_of_rows)
-    # One way to make this code more flexible to e.g. handle multiple categories
-    # would be to return a list of "swarmified" y coordinates here and then plot
-    # outside the function.
-    # That generalization would let you "swarmify" y coordinates for each
-    # category and add category specific offsets to put the each category in its
-    # own row
-
-    fig = px.scatter(
-        df,
-        x="x",
-        y="y",
-        title=fig_title,
-    )
-    # we want to suppress the y coordinate in the hover value because the
-    # y-coordinate is irrelevant/misleading
-    fig.update_traces(
-        marker_size=point_size,
-        # suppress the y coordinate because the y-coordinate is irrelevant
-        hovertemplate="<b>value</b>: %{x}",
-    )
-    # we have to set the width and height because we aim to avoid icon collisions
-    # and we specify the icon size in the same units as the width and height
-    fig.update_layout(width=fig_width, height=(
-        point_size*max(bin_counter.values())+200))
-    fig.update_yaxes(
-        showticklabels=False,  # Turn off y-axis labels
-        ticks='',               # Remove the ticks
-        title=""
-    )
-    return fig
 
 def swarm_df(
     X_series,
@@ -334,12 +227,32 @@ if __name__ == '__main__':
 
         st.markdown(f'# Patient')
         selected_patient = st.selectbox(label=f'Please select patient to display ({number_of_patients})', options=summary_metrics['Patient'].unique())
-        with st.expander('Patient OAR Selection'):
+        with st.expander('Patient Contours Selection'):
+            treatment_planning_contours = ['CTV_High', 'PTV_High']
+            button1_loc, button2_loc, button3_loc = st.columns([1,1,1])
             specific_patient_df = summary_metrics[(summary_metrics['Patient'] == selected_patient)]
             oar_list = specific_patient_df['OAR'].unique().tolist()
             oar_selection = pd.DataFrame({"OAR": oar_list,"Display": [True]*len(oar_list),})
+            with button1_loc:
+                if st.button('All', width='stretch'):
+                    oar_selection['Display'].values[:] = True
+            with button2_loc:
+                if st.button('Planning', width='stretch'):
+                    for name, val in oar_selection.values:
+                        if (name in treatment_planning_contours):
+                            oar_selection.loc[oar_selection['OAR']==name, 'Display'] = True
+                        else:
+                            oar_selection.loc[oar_selection['OAR']==name, 'Display'] = False
+            with button3_loc:
+                if st.button('OARs', width='stretch'):
+                    for name, val in oar_selection.values:
+                        if (name not in treatment_planning_contours):
+                            oar_selection.loc[oar_selection['OAR']==name, 'Display'] = True
+                        else:
+                            oar_selection.loc[oar_selection['OAR']==name, 'Display'] = False
+
             updated_selection = st.data_editor(oar_selection,
-                                                column_config={"Display": st.column_config.CheckboxColumn("Display?",default=True,)},
+                                                column_config={"Display": st.column_config.CheckboxColumn("Display?")},
                                                 hide_index=True)
         oar_list = updated_selection['OAR'][updated_selection['Display'] == True]
         st.markdown(f'# Population')
